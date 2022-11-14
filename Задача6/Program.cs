34765ad1070f8cfb5f082f176dbae31289b91038
«Vadim <vvaganoff@mail.ru>
2022-11-14 22:53:13 +0400

    добавлено решение задачи номер 6

---
+++ Задача6/Program.cs
@@ -0,0 +1,4 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32( Console.ReadLine() );
+if ( num % 2 == 0 ) Console.WriteLine("Это число является четным");
+else Console.WriteLine("Это число является нечетным");
