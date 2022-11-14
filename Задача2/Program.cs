25bb7fea5293052073130f406e046ed1967ed1fa
«Vadim <vvaganoff@mail.ru>
2022-11-14 22:28:08 +0400

    обавлено решение задачи номер 2

---
+++ Задача2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write( "Введите первое число: " );
+int num1 = Convert.ToInt32( Console.ReadLine() );
+Console.Write( "Введите второе число: " );
+int num2 = Convert.ToInt32( Console.ReadLine() );
+
+if (num1 > num2) 
+{
+    Console.WriteLine ( "Число " + num1 + " больше, чем " + num2 );
+    Console.WriteLine ( "Число " + num2 + " меньше, чем " + num1 );
+}
+else if (num2 > num1) 
+{
+    Console.WriteLine ( "Число " + num2 + " больше, чем " + num1 );
+    Console.WriteLine ( "Число " + num1 + " меньше, чем " + num2 );
+}
+else Console.WriteLine( "Эти числа равны" );
